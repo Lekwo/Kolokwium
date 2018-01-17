47f4bf3eec60560d3fcef1900432e2f2ecb651ea
Lekwo <lekwo7@o2.pl>
2018-01-17 18:53:36 +0100

    Add files via upload

---
+++ kolokwium/Europa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kolokwium
+{
+    class Europa:ISprawdzanie
+    {
+        private SortedList<Kraj, Stolica> panstwa = new SortedList<Kraj,Stolica>();
+
+        public void Dodaj(string kraj, int powierzchnia, string stolica, bool lotnisko)
+        {
+            panstwa.Add(Kraj.UtworzKraj(kraj, powierzchnia), new Stolica(stolica, lotnisko));
+        }
+
+        public bool Sprawdz(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public void Usun()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Usun(string kraj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ kolokwium/IOperacje.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kolokwium
+{
+    interface IOperacje
+    {
+        void Dodaj(string kraj, int powierzchnia, string stolica, bool lotnisko);
+        void Usun();
+        void Usun(string kraj);
+    }
+}
